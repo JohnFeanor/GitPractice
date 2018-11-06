b7b842d0c16388c651c7e9593cd413f53ed16aa5
JohnFeanor <john@feanor.com.au>
2018-11-07 08:51:28 +1000

    Initial commit

---
+++ GitPractice/GitPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by John Sandercock on 7/11/18.
+//  Copyright © 2018 Feanor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
